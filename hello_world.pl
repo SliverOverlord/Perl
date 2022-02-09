6db0a0696dec92f86d86d2304fb453c4274dfc63
Turtle <deniojo@mnstate.edu>
2022-02-09 15:19:57 -0600

    First commit to perl folder.

---
+++ hello_world.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+#Author: Joshua DeNio
+#Date: 02/08/2022
+#Description: This is hello world in perl.
+#To Run: perl hello_world.pl
+
+#Test the print command in perl
+print("Hello world\n");
+
