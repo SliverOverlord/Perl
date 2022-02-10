27bd8a24cded9681ff00ce30fd64880a8a0aaf92
Turtle <deniojo@mnstate.edu>
2022-02-09 19:00:27 -0600

    Working with multiline comments in perl

---
+++ multiline_comment.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+=begin
+Author: Joshua DeNio
+Date: 02/08/2022
+Description: A perl program to test block comments, comments such.
+=cut
+
+print("Hello World\n");
+
+=begin
+this should be a comment
+=cut
+
+print("ok\n");
